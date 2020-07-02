dcb689dc979b66fdce727512ee47c91e5c766de6
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-07-02 01:21:37 -0500

    Initial solution structure

---
+++ src/Queryology.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Queryology.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
