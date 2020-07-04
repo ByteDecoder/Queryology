7b763c998f82352108189dc6541b7b515dacfccc
ByteCoder <rreyes@canaryhealth.com>
2020-07-04 05:08:14 -0500

    Implementin IDisposable for QueryologyEngineTestBuilder

--- src/Queryology.Tests/TestBuilders/QueryologyEngineTestBuilder.cs
+++ src/Queryology.Tests/TestBuilders/QueryologyEngineTestBuilder.cs
@@ -1,10 +1,12 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 
 namespace ByteDecoder.Queryology.Tests.TestBuilders
 {
-  internal class QueryologyEngineTestBuilder<T> where T : DbContext
+  internal class QueryologyEngineTestBuilder<T> : IDisposable where T : DbContext
   {
     private readonly QueryologyEngine<T> _queryologyEngine;
+    private bool disposedValue;
 
     public QueryologyEngineTestBuilder(T dbContext)
     {
@@ -26,6 +28,35 @@ namespace ByteDecoder.Queryology.Tests.TestBuilders
     public QueryologyEngine<T> Build()
     {
       return _queryologyEngine;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+      if (!disposedValue)
+      {
+        if (disposing)
+        {
+          // TODO: eliminar el estado administrado (objetos administrados)
+        }
+
+        // TODO: liberar los recursos no administrados (objetos no administrados) y reemplazar el finalizador
+        // TODO: establecer los campos grandes como NULL
+        disposedValue = true;
+      }
+    }
+
+    // // TODO: reemplazar el finalizador solo si "Dispose(bool disposing)" tiene código para liberar los recursos no administrados
+    // ~QueryologyEngineTestBuilder()
+    // {
+    //     // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+    //     Dispose(disposing: false);
+    // }
+
+    public void Dispose()
+    {
+      // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+      Dispose(disposing: true);
+      GC.SuppressFinalize(this);
     }
   }
 }
