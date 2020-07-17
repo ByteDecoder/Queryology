876ea356a4305e2ea77904633cbf0341ce29112b
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-07-17 03:02:04 -0500

    Added QueryologyEngineBuilder<T>

---
+++ src/Queryology/IQueryologyEngineBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ByteDecoder.Queryology
+{
+  public interface IQueryologyEngineBuilder<T> where T : DbContext
+  {
+    IQueryologyEngine<T> Build();
+  }
+}
---
+++ src/Queryology/QueryologyEngineBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ByteDecoder.Queryology.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace ByteDecoder.Queryology
+{
+  public class QueryologyEngineOptions<T> where T : DbContext
+  {
+    public T DataContextProvider { get; set; }
+    public IObjectDisplayer ObjectDisplayerProvider { get; set; }
+  }
+
+  public class QueryologyEngineBuilder<T> : IQueryologyEngineBuilder<T> where T : DbContext
+  {
+    private IQueryologyEngine<T> _queryologyEngine;
+
+    public IQueryologyEngineBuilder<T> Configure(Action<QueryologyEngineOptions<T>> queryologyEngineOptions)
+    {
+      var options = new QueryologyEngineOptions<T>();
+      queryologyEngineOptions(options);
+      _queryologyEngine = new QueryologyEngine<T>(options.DataContextProvider, options.ObjectDisplayerProvider);
+
+      return this;
+    }
+
+    public IQueryologyEngine<T> Build()
+    {
+      return _queryologyEngine;
+    }
+  }
+}
