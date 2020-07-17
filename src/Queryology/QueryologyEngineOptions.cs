0ba3057404dd83e2fe60e6d10c67d2471803ab41
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-07-17 13:31:32 -0500

    Extending QueryologyEngineOptions<T>  with more tehc features

--- src/Queryology/QueryologyEngineOptions.cs
+++ src/Queryology/QueryologyEngineOptions.cs
@@ -1,3 +1,4 @@
+using System;
 using ByteDecoder.Queryology.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,5 +21,21 @@ namespace ByteDecoder.Queryology
     /// </summary>
     /// <value></value>
     public IObjectDisplayer ObjectDisplayerProvider { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public QueryologyEngineOptions() { }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="dataContextProvider"></param>
+    /// <param name="objectDisplayer"></param>
+    public QueryologyEngineOptions(T dataContextProvider, IObjectDisplayer objectDisplayer)
+    {
+      DataContextProvider = dataContextProvider ?? throw new ArgumentNullException(nameof(dataContextProvider));
+      ObjectDisplayerProvider = objectDisplayer ?? throw new ArgumentNullException(nameof(objectDisplayer));
+    }
   }
 }
