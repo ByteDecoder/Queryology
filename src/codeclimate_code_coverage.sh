16e5abb61d4c7b567c7bfe4b69f7b4bc529793be
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-07-20 12:52:52 -0500

    problem with github actions

--- src/codeclimate_code_coverage.sh
+++ src/codeclimate_code_coverage.sh
@@ -9,11 +9,9 @@ dotnet add package coverlet.msbuild
 # Start codeclimate process
 ./codeclimate-test-reporter before-build
 
-echo "Running test"
-
 # Running unit tests - 'lcov' output format (and put coverage in correct path)
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=coverage /p:Exclude=[xunit.*]* ./Queryology.sln --no-restore --verbosity normal
 mkdir coverage && mv ./Queryology.Tests/coverage.info coverage/lcov.info
 
 # Send test report result to codeclimate
-./codeclimate-test-reporter after-build -t lcov -r ${{ secrets.CC_TEST_REPORTER_ID }} -p ./ --exit-code $?
+./codeclimate-test-reporter after-build -t lcov -r ${CC_TEST_REPORTER_ID} -p ./ --exit-code $?
