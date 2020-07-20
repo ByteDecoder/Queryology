60e30e3d5c528b01c00048303d7b069e48bf5d9c
ByteDecoder <rreyes@canaryhealth.com>
2020-07-20 02:17:59 -0500

    ObjectDumper refactored

--- src/Queryology.Providers.ObjectDumper/ObjectDumper.cs
+++ src/Queryology.Providers.ObjectDumper/ObjectDumper.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
@@ -37,7 +38,7 @@ namespace ByteDecoder.Queryology.Providers.ObjectDumper
     /// <param name="log"></param>
     public static void Write(object element, int depth, TextWriter log)
     {
-      var dumper = new ObjectDumper(depth) {_writer = log};
+      var dumper = new ObjectDumper(depth) { _writer = log };
       dumper.WriteObject(null, element);
     }
 
@@ -134,29 +135,30 @@ namespace ByteDecoder.Queryology.Providers.ObjectDumper
           }
 
           if (propWritten) WriteLine();
-          if (_level < _depth)
-          {
-            foreach (MemberInfo m in members)
-            {
-              FieldInfo f = m as FieldInfo;
-              PropertyInfo p = m as PropertyInfo;
-              if (f != null || p != null)
-              {
-                Type t = f != null ? f.FieldType : p.PropertyType;
-                if (!(t.IsValueType || t == typeof(string)))
-                {
-                  object value = f != null ? f.GetValue(element) : p.GetValue(element, null);
-                  if (value != null)
-                  {
-                    _level++;
-                    WriteObject(m.Name + ": ", value);
-                    _level--;
-                  }
-                }
-              }
-            }
-          }
+          DepthWriter(element, members);
         }
+      }
+    }
+
+    private void DepthWriter(object element, IEnumerable<MemberInfo> members)
+    {
+      if (_level >= _depth) return;
+
+      foreach (var m in members)
+      {
+        var f = m as FieldInfo;
+        var p = m as PropertyInfo;
+
+        if (f == null && p == null) continue;
+        var t = f != null ? f.FieldType : p.PropertyType;
+
+        if (t.IsValueType || t == typeof(string)) continue;
+        var value = f != null ? f.GetValue(element) : p.GetValue(element, null);
+
+        if (value == null) continue;
+        _level++;
+        WriteObject(m.Name + ": ", value);
+        _level--;
       }
     }
 
