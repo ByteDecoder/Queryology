1e83aee7453bd19e9d3e7c3c96f44e0f5dbbe1bc
ByteDecoder <rreyes@canaryhealth.com>
2020-07-20 03:31:28 -0500

    sempahore ci fix

--- src/codeclimate_code_coverage.sh
+++ src/codeclimate_code_coverage.sh
@@ -9,11 +9,11 @@ dotnet add package coverlet.msbuild
 # Start codeclimate process
 ./codeclimate-test-reporter before-build
 
-echo "Running test suite"
+echo "Running test"
 
 # Running unit tests - 'lcov' output format (and put coverage in correct path)
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=coverage /p:Exclude=[xunit.*]* ./Queryology.sln --no-restore --verbosity normal
 mkdir coverage && mv ./Queryology.Tests/coverage.info coverage/lcov.info
 
 # Send test report result to codeclimate
-./codeclimate-test-reporter after-build -t lcov -r ${4756e012cbc04ec5e5138b27e2f8800832fd6645c1e59ff0e6baed170f92242e} -p ./ --exit-code $?
+./codeclimate-test-reporter after-build -t lcov -r 4756e012cbc04ec5e5138b27e2f8800832fd6645c1e59ff0e6baed170f92242e -p ./ --exit-code $?
