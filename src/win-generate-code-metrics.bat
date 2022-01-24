fcb41c6d457c927787a4e3a9c5bd115854a89730
ByteDecoder <rreyes@canaryhealth.com>
2022-01-23 22:12:58 -0600

    Added code metrics calculation with the CLI

---
+++ src/win-generate-code-metrics.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+cls
+echo Generating  Code Metrics
+echo
+dotnet build .\Queryology\ -c Release /t:Metrics /p:MetricsOutputFile=../.code-metrics/Queryology.Metrics.xml
+
+echo We are Legion
