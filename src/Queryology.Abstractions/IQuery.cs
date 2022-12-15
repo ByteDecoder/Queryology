401c37a1dd53decbc00d20003cc7be4b863576df
ByteDecoder <rreyes@canaryhealth.com>
2022-12-14 19:02:16 -0600

    Code updates

--- src/Queryology.Abstractions/IQuery.cs
+++ src/Queryology.Abstractions/IQuery.cs
@@ -1,48 +1,48 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace ByteDecoder.Queryology.Abstractions
+namespace ByteDecoder.Queryology.Abstractions;
+
+/// <summary>
+/// Method for displaying the data. This delagate can be used for web, console,
+/// files or other storages types to store the result.
+/// </summary>
+/// <param name="title"></param>
+/// <param name="data"></param>
+/// <param name="depth"></param>
+public delegate void DisplayObjectData(string title, object data, int depth);
+
+/// <summary>
+/// Describes the contract for IQuery.
+/// </summary>
+/// <typeparam name="T">DbContext Type.</typeparam>
+public interface IQuery<T>
+    where T : DbContext
 {
     /// <summary>
-    /// Method for displaying the data. This delagate can be used for web, console,
-    /// files or other storages types to store the result.
+    /// Gets or sets the reference to the EF Core DbContext class allowed to use in the query.
     /// </summary>
-    /// <param name="title"></param>
-    /// <param name="data"></param>
-    /// <param name="depth"></param>
-    public delegate void DisplayObjectData(string title, object data, int depth);
+    /// <value>DbContext.</value>
+    T DataContext { get; set; }
 
     /// <summary>
-    /// Describes the contract for IQuery.
+    /// Gets the data object processed.
     /// </summary>
-    /// <typeparam name="T">DbContext Type.</typeparam>
-    public interface IQuery<T>
-        where T : DbContext
-    {
-        /// <summary>
-        /// Gets or sets the reference to the EF Core DbContext class allowed to use in the query.
-        /// </summary>
-        /// <value>DbContext.</value>
-        T DataContext { get; set; }
+    /// <value>Data object.</value>
+    object Data { get; }
 
-        /// <summary>
-        /// Gets the data object processed.
-        /// </summary>
-        /// <value>Data object.</value>
-        object Data { get; }
+    /// <summary>
+    /// Gets a value indicating whether gets if the query can be executed.
+    /// </summary>
+    bool Executable { get; }
 
-        /// <summary>
-        /// Gets a value indicating whether gets if the query can be executed.
-        /// </summary>
-        bool Executable { get; }
+    /// <summary>
+    /// Gets or sets the IObjectDisplayer.
+    /// </summary>
+    DisplayObjectData ObjectDisplayer { get; set; }
 
-        /// <summary>
-        /// Gets or sets the IObjectDisplayer.
-        /// </summary>
-        DisplayObjectData ObjectDisplayer { get; set; }
-
-        /// <summary>
-        /// Execute the query main logic body.
-        /// </summary>
-        void Execute();
-    }
+    /// <summary>
+    /// Execute the query main logic body.
+    /// </summary>
+    void Execute();
 }
+
--- src/Queryology.Abstractions/QueryologyEngineOptions.cs
+++ src/Queryology.Abstractions/QueryologyEngineOptions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 namespace ByteDecoder.Queryology.Abstractions;
+
 /// <summary>
 ///
 /// </summary>
